1b3320216c4c7133825c508982ee980bc56a9a7c
Isabel RM <isabelroche@uma.es>
2017-03-14 20:07:47 +0100

    Últimos cambios y correcciones

--- Lib.java
+++ Lib.java
@@ -1,36 +1,30 @@
 /**
  *
  * @author Isabel Roche Márquez
-	@Colaborador Aitor Gustavo Camacho Gutiérrez
+     @Colaborador Aitor Gustavo Camacho Gutiérrez
  */
 
 public class Lib {
 
-    public void metodoAlumno1(){
-        System.out.println("metodo 1");
-        System.out.println("modificado por alumno 1"); // alumno 1
+    public void metodoIsabel(){
+        System.out.println("modificado por Isabel"); // Modificado por el alumno propietario
     }
 
     public void metodoAitorGustavo(){
-       System.out.println("Modificado por Aitor "); //Modificado por el alumno colaborador
+       System.out.println("Modificado por Aitor"); //Modificado por el alumno colaborador
     }
 
     public void metodoComunitario(){
-    } //Comentario incluido para pruebas
+          //Comentario incluido para pruebas
+          System.out.println ("Aquí escribimos todos);
 
-System.out.println ("Aquí escribimos todos);
+          // Modificaciones alumno propietario
+          System.out.println("Isabel inserta esta sentencia");
 
-// Modificaciones alumno 1
-        System.out.println("Alumno 1 inserta esta sentencia");
+          //Modificaciones alumno colaborador
+          System.outprintln("Aitor inserta esta sentencia");
+     }
 
-//Modificaciones alumno colaborador
-System.outprintln("Aitor inserta esta sentencia");
-
-
-    }
-
-         System.out.println("Aqui escribimos todos");
-       
-  
+         System.out.println("Fin");
 }
 
